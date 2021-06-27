7e8121af4194d4cd6a28a3d767f8bac98a10da71
yuri-misonoo <yuri.panpanda@gmail.com>
2021-06-27 05:08:11 +0000

    [Finish]3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   
   def show
+    @blog = Blog.find(params[:id])
   end 
   
   def index
@@ -14,7 +15,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blogs_path(blog.id)
   end
 
   def edit
